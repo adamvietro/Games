9472e5a4654a45ecae8c240c1fbf9d8a4c40bd4c
Adam V. <hunterafi@gmail.com>
2025-01-18 19:10:46 -0800

    Added High or Low!

--- lib/games.ex
+++ lib/games.ex
@@ -20,11 +20,22 @@ defmodule Games do
     IO.gets("Incorrect!\nEnter your guess: ") |> String.trim()
   end
 
+  def too_low do
+    IO.gets("Too low!\nEnter your guess: ") |> String.trim()
+  end
+
+  def too_high do
+    IO.gets("Too high!\nEnter your guess: ") |> String.trim()
+  end
+
   def test_guess(guess, answer) do
     cond do
       guess == answer -> "You win!"
-      guess != answer ->
-        guess = user_input_incorrect()
+      guess > answer ->
+        guess = too_high()
+        test_guess(guess, answer)
+      guess < answer ->
+        guess = too_low()
         test_guess(guess, answer)
     end
   end
