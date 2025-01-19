f3471fa25cc23d481927e6012c0e087fb5005819
Adam V. <hunterafi@gmail.com>
2025-01-18 18:37:20 -0800

    Game is working

--- lib/games.ex
+++ lib/games.ex
@@ -1,18 +1,39 @@
 defmodule Games do
   @moduledoc """
-  Documentation for `Games`.
+  This will be a guessing game that will have a user guess a number from
+  1 - 10
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Games.hello()
-      :world
 
   """
-  def hello do
-    :world
+  def user_input do
+    IO.gets("Guess a number between 1 and 10:")
+  end
+
+  # def user_input(false) do
+  #   IO.gets("Incorrect!\n Enter your guess:")
+  # end
+
+  def user_input(false) do
+    "Inncorrect!"
+  end
+
+  def user_input(true) do
+    "You win!"
+  end
+
+  def random_number() do
+    Enum.random(1..10)
+  end
+
+  def play do
+    answer = random_number()
+    guess = user_input()
+
+    cond do
+      guess == answer -> user_input(true)
+      true -> user_input(false)
+    end
   end
 end
