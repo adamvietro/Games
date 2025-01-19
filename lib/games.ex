3393fa10b8aea8be786457be44534e8c0ce5b7b8
Adam V. <hunterafi@gmail.com>
2025-01-18 19:27:22 -0800

    Added Limited Attempts

--- lib/games.ex
+++ lib/games.ex
@@ -2,8 +2,12 @@ defmodule Games do
   @moduledoc """
   This will be a guessing game that will have a user guess a number from
   1 - 10
+
+  added
+  1. Re-Attempt
+  2. Too Low/High
+  3. Limited attempts
   """
-  alias Logger.Backends.Internal
 
   @doc """
 
@@ -12,13 +16,17 @@ defmodule Games do
     Enum.random(1..10) |> Integer.to_string()
   end
 
-  def user_input do
+  def user_guess do
     IO.gets("Guess a number between 1 and 10: ") |> String.trim()
   end
 
-  def user_input_incorrect do
-    IO.gets("Incorrect!\nEnter your guess: ") |> String.trim()
+  def number_of_guesses do
+    Enum.random(1..10)
   end
+
+  # def user_input_incorrect do
+  #   IO.gets("Incorrect!\nEnter your guess: ") |> String.trim()
+  # end
 
   def too_low do
     IO.gets("Too low!\nEnter your guess: ") |> String.trim()
@@ -28,24 +36,31 @@ defmodule Games do
     IO.gets("Too high!\nEnter your guess: ") |> String.trim()
   end
 
-  def test_guess(guess, answer) do
+  def test_guess(guess, answer, number_of) do
     cond do
-      guess == answer -> "You win!"
-      guess > answer ->
+      guess == answer ->
+        IO.puts("You win!")
+
+      number_of == 0 ->
+        IO.puts("You lose! the answer was #{answer}")
+
+      guess > answer and number_of > 0 ->
         guess = too_high()
-        test_guess(guess, answer)
-      guess < answer ->
+        test_guess(guess, answer, number_of - 1)
+
+      guess < answer and number_of > 0 ->
         guess = too_low()
-        test_guess(guess, answer)
+        test_guess(guess, answer, number_of - 1)
     end
   end
 
   def play do
+    IO.puts("Let's play a game. I'll pick a number and a random number of guesses, can you figure it out?")
     answer = random_number()
-    guess = user_input()
-    # IO.inspect(answer, label: "answer")
-    # IO.inspect(guess, label: "guess")
+    number_of = number_of_guesses()
+    guess = user_guess()
 
-    test_guess(guess, answer)
+
+    test_guess(guess, answer, number_of)
   end
 end
